42cebcca5cf6c843cc7abcaa28112bd459631494
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-26 18:00:00 +0600

    5th commi

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+  const provider = new GoogleAuthProvider();
+  import {auth} from '../../Firebase/Firebase.init';
 
 const Login = () => {
+      const handleProviderLogin=()=>{
+            signInWithPopup(auth,provider)
+            .then(result=>{
+                  console.log(result.user);
+            })
+            .catch(error=>{
+                  console.log(error);
+            })
+      }
+    
       return (
             <div>
                 <h3>This is Login</h3>  
+                <button onClick={handleProviderLogin}>Sign In With Google</button>
             </div>
       );
 };
