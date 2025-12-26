cbc245385d9f9923d1178eb79beecb3074f9f650
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-26 18:59:21 +0600

    6th commit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import React, { useState } from 'react';
+import { GoogleAuthProvider, signInWithPopup,signOut } from "firebase/auth";
   const provider = new GoogleAuthProvider();
   import {auth} from '../../Firebase/Firebase.init';
 
 const Login = () => {
+      const [user,setUser]=useState(null);
       const handleProviderLogin=()=>{
             signInWithPopup(auth,provider)
             .then(result=>{
                   console.log(result.user);
+                  setUser(result.user)
             })
             .catch(error=>{
                   console.log(error);
+            })
+      }
+      const handlesignOut=()=>{
+            signOut(auth).
+            then(()=>{
+                  console.log("Sign out Done")
+                  setUser(null);
             })
       }
     
       return (
             <div>
                 <h3>This is Login</h3>  
-                <button onClick={handleProviderLogin}>Sign In With Google</button>
+               
+                
+                {
+                  user ? 
+                  
+                   <button onClick={handlesignOut}>SIgn out</button>
+                  : <button onClick={handleProviderLogin}>Sign In With Google</button>
+                }
+                <div>
+                  <h2>{user?.displayName}</h2>
+                  <h4>{user?.email}</h4>
+                  <img src={user?.photoURL}alt="" />
+                </div>
             </div>
+            
       );
 };
 
